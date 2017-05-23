db43c207d7a23c745ef583cf29629bcad3a269f3
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2017-05-24 08:18:33 +0900

    feat(components): update <app-main-card> with record data

--- src/app/components/main-card/main-card.component.ts
+++ src/app/components/main-card/main-card.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { record } from '../../mocks/record';
+
 
 @Component({
   selector: 'app-main-card',
@@ -6,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-card.component.scss']
 })
 export class MainCardComponent implements OnInit {
+
+  // this is data used to display
+  data = record;
 
   constructor() { }
 
