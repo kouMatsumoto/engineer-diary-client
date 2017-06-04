7f48e1207d8f69a190115053b35bc93f142b3dfc
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2017-06-04 19:12:47 +0900

    refactor: declare `RecordService` as module provider and remove from each component

--- src/app/components/record-update-page/record-update-page.component.ts
+++ src/app/components/record-update-page/record-update-page.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Record } from '../../types/record';
-import { RecordService } from '../../services/record.service';
 
 @Component({
   selector: 'app-record-update-page',
   templateUrl: './record-update-page.component.html',
-  styleUrls: ['./record-update-page.component.scss'],
-  providers: [RecordService]
+  styleUrls: ['./record-update-page.component.scss']
 })
 export class RecordUpdatePageComponent implements OnInit {
   record: Record;
